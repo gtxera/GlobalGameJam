6abbd0da382187e366a681719b362bd2c3ab6b92
Gabriel Teixeira <55255028+gtxera@users.noreply.github.com>
2023-02-03 19:22:40 -0300

    FÉ

--- GlobalGameJam/Assets/Scripts/Player/PlayerWallInteraction.cs
+++ GlobalGameJam/Assets/Scripts/Player/PlayerWallInteraction.cs
@@ -36,25 +36,35 @@ public class PlayerWallInteraction : MonoBehaviour
         var raycastResults = WallRaycast();
         IsCollidingWithWall = raycastResults.Collided;
         ContactDirection = raycastResults.CollisionDirection;
+        
     }
 
     private WallRaycastResults WallRaycast()
     {
-        RaycastHit2D leftHitUp = (Physics2D.Raycast(transform.position + _raycastOffsetLeftUp, transform.TransformDirection(Vector3.right), _extraRaycastSize, _layerMask));
-        RaycastHit2D leftHitDown = (Physics2D.Raycast(transform.position + _raycastOffsetLeftDown, transform.TransformDirection(Vector3.right), _extraRaycastSize,_layerMask));
-        RaycastHit2D rightHitUp = (Physics2D.Raycast(transform.position + _raycastOffsetRightUp, transform.TransformDirection(Vector3.left), _extraRaycastSize,_layerMask));
-        RaycastHit2D rightHitDown = (Physics2D.Raycast(transform.position + _raycastOffsetRightDown, transform.TransformDirection(Vector3.left), _extraRaycastSize,_layerMask));
+        RaycastHit2D leftHitUp = (Physics2D.Raycast(transform.position + _raycastOffsetLeftUp, transform.TransformDirection(Vector3.left), _extraRaycastSize, _layerMask));
+        RaycastHit2D leftHitDown = (Physics2D.Raycast(transform.position + _raycastOffsetLeftDown, transform.TransformDirection(Vector3.left), _extraRaycastSize,_layerMask));
+        RaycastHit2D rightHitUp = (Physics2D.Raycast(transform.position + _raycastOffsetRightUp, transform.TransformDirection(Vector3.right), _extraRaycastSize,_layerMask));
+        RaycastHit2D rightHitDown = (Physics2D.Raycast(transform.position + _raycastOffsetRightDown, transform.TransformDirection(Vector3.right), _extraRaycastSize,_layerMask));
 
         if (leftHitUp && leftHitDown)
         {
-            if (leftHitUp.transform.CompareTag("Ground") && leftHitDown.transform.CompareTag("Ground")) return new WallRaycastResults() { Collided = true, CollisionDirection = 1 }; 
+            if (leftHitUp.transform.CompareTag("Ground") && leftHitDown.transform.CompareTag("Ground")) return new WallRaycastResults() { Collided = true, CollisionDirection = -1 }; 
         }
         
         else if (rightHitUp && rightHitDown)
         {
-            if (rightHitUp.transform.CompareTag("Ground") && rightHitDown.transform.CompareTag("Ground")) return new WallRaycastResults() { Collided = true, CollisionDirection = -1 }; 
+            if (rightHitUp.transform.CompareTag("Ground") && rightHitDown.transform.CompareTag("Ground")) return new WallRaycastResults() { Collided = true, CollisionDirection = 1 }; 
         }
 
         return new WallRaycastResults(){Collided = false, CollisionDirection = 0}; 
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position + _raycastOffsetLeftUp, transform.position + _raycastOffsetLeftUp + _extraRaycastSize * transform.TransformDirection(Vector3.left));
+        Gizmos.DrawLine(transform.position + _raycastOffsetLeftDown, transform.position + _raycastOffsetLeftDown + _extraRaycastSize * transform.TransformDirection(Vector3.left));
+        Gizmos.DrawLine(transform.position + _raycastOffsetRightUp, transform.position + _raycastOffsetRightUp + _extraRaycastSize * transform.TransformDirection(Vector3.right));
+        Gizmos.DrawLine(transform.position + _raycastOffsetRightDown, transform.position + _raycastOffsetRightDown + _extraRaycastSize * transform.TransformDirection(Vector3.right));
     }
 }
